9c347b2aa4bbb805f62e165d24eeca1099071ab9
Kudryavka <Kudryavka03@outlook.com>
2025-03-27 13:03:31 +0800

    添加项目文件。

---
+++ UECap Feature Parser/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace UECap_Feature_Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            textBox2 = new System.Windows.Forms.TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(12, 40);
+            textBox1.MaxLength = 0;
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            textBox1.Size = new System.Drawing.Size(1358, 249);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(17, 15);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(520, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Paste your NSG UECap Singal Text here. Only Support SA, Not Support EN-DC";
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(12, 295);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Analysis";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(12, 330);
+            textBox2.MaxLength = 0;
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            textBox2.Size = new System.Drawing.Size(1358, 565);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1382, 907);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "UECap Feature Parser";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ UECap Feature Parser/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UECap_Feature_Parser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // getBandCombiantionList();
+
+            getBandCombiantionList();
+
+        }
+        public int convertTextToInt(string text)
+        {
+            Debug.WriteLine(text);
+            switch (text) {
+                case "a": return 1;
+                    case "c": return 2;
+                    case "d": return 3;
+                    case "e": return 4;
+                 default: return 0;
+            }
+
+        }
+        public bool getSrsCs(int a)
+        {
+            var start = textBox1.Text.IndexOf("supportedBandCombinationList-v1540");
+            var stop = textBox1.Text.IndexOf("srs-SwitchingTimeRequested");
+            try
+            {
+                String result = textBox1.Text.Substring(start, stop - start);
+                var array = result.Split("bandList-v1540");
+                try { if (array[a].IndexOf("srs-CarrierSwitch") != -1) return true; }
+                catch { return false; }
+            }
+            catch { return false; }
+            return false;
+        }
+        public  void getBandCombiantionList()
+        {
+            var nr_start = textBox1.Text.IndexOf("supportedBandListNR");
+            var nr_result = textBox1.Text.Substring(nr_start, textBox1.TextLength - nr_start);
+            var start = nr_result.IndexOf("supportedBandCombinationList");
+            var stop = nr_result.IndexOf("appliedFreqBandListFilter");
+            String result = nr_result.Substring(start, stop-start);
+            var array = result.Split("[0] -> nr");
+            int bandIndex = 0;
+            List<List<OneBandCombinationFeature>> AllTable = new List<List<OneBandCombinationFeature>>();
+            foreach (var item in array) // 这里是每一个聚合的列表
+            {
+                List<OneBandCombinationFeature> oneBandCombinationFeature = new List<OneBandCombinationFeature>();
+                var bandCombiantion = item.Split("bandNR : ");
+                if (bandCombiantion[0] != item)
+                {
+                    foreach (var item2 in bandCombiantion)  // item2已经是核心内容乐，通常情况下只会存在一个ca-BandwidthClassDL-NR，此时逐行扫描即可
+                    {
+                        //if (item2 == "")
+                        try
+                        {
+                            OneBandCombinationFeature bandInfo = new OneBandCombinationFeature();
+                            var resultArray = item2.Split("\r\n");
+                            bandInfo.bandNum = Convert.ToInt32(resultArray[0]);
+
+                            foreach (var item3 in resultArray)
+                            {
+                                if (item3.IndexOf("ca-BandwidthClassDL-NR") != -1) bandInfo.DL = item3.Split(":")[1];
+                                if (item3.IndexOf("ca-BandwidthClassUL-NR") != -1) bandInfo.UL = item3.Split(":")[1];
+                            }
+                            bandInfo.bandIndex = bandIndex;
+                            oneBandCombinationFeature.Add(bandInfo);
+                        }
+                        catch { }
+                    }
+                    
+                }
+                //这里oneBandCombinationFeature已经收集完这个组合的每一个band信息，接下来就是汇总
+                AllTable.Add(oneBandCombinationFeature);
+                bandIndex++;
+                
+
+
+            }
+            String lastResult = "";
+            var bandCaIndex = 0;
+            foreach (var item in AllTable)
+            {
+                
+                foreach(var item2 in item)
+                {
+                    if (item2.bandNum == 0) break;
+                    lastResult += $"{item2.bandNum}{item2.DL}{item2.UL} + ";
+                    
+                }
+                if (lastResult.Length != 0) lastResult += ("SRS CS:"+getSrsCs(bandCaIndex) + "\r\n");
+                bandCaIndex++;
+            }
+            textBox2.Text = lastResult.Replace(" ","");
+            var a = 0;
+        }
+    }
+
+    /*
+    public class AllBandCombinationListFeature { 
+        List<>
+    }
+    */
+    public class OneBandCombinationListFeature
+    {
+          List<OneBandCombinationFeature> features = new List<OneBandCombinationFeature>();
+        
+    }
+    public class OneBandCombinationFeature
+    {
+        public int bandIndex = 0;
+        public int bandNum = 0;
+        public string UL = "";
+        public string DL = "";
+    }
+
+}
